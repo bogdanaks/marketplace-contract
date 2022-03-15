2a71170d13e3010c7beb87e3ed75009e2ddb8284
bogdanaksenov <bogdanaks@bk.ru>
2022-03-16 00:34:26 +0300

    Update

--- contracts/MarketPlace.sol
+++ contracts/MarketPlace.sol
@@ -15,9 +15,19 @@ contract MarketPlace is Ownable {
         RUNNING,
         FINISHED
     }
+    enum OrderType {
+        FIX_PRICE,
+        AUCTION
+    }
+
+    struct Bid {
+        uint256 amount;
+        address bidder;
+        uint256 createdAt;
+    }
 
     struct Order {
-        uint256 tokenId; 
+        OrderType orderType;
         OrderStatus status;
         uint256 price;
         address owner;
@@ -25,6 +35,12 @@ contract MarketPlace is Ownable {
     }
 
     mapping(uint256 => Order) public orders;
+    mapping(uint256 => Bid[]) public bids;
+
+    modifier onlyOrderByType(uint256 _tokenId, OrderType _orderType) {
+        require(orders[_tokenId].orderType == _orderType, "Invalid order type");
+        _;
+    }
 
     function createItem(string memory _tokenURI, address _owner) public {
         createNFT(_owner, _tokenURI);
@@ -34,12 +50,12 @@ contract MarketPlace is Ownable {
     function listItem(uint256 _tokenId, uint256 _price) public {
         require(orders[_tokenId], "Already exist");
 
-        orders[_tokenId] = new Order(_tokenId, OrderStatus.PENDING, msg.sender, block.timestamp);
+        orders[_tokenId] = new Order(OrderType.FIX_PRICE, OrderStatus.PENDING, _price, msg.sender, block.timestamp);
         transferNFT(msg.sender, address(this), _tokenId);
         // TODO add event
     }
 
-    function buyItem(uint256 _tokenId) public {
+    function buyItem(uint256 _tokenId) public onlyOrderByType(_tokenId, OrderType.FIX_PRICE) {
         require(orders[_tokenId], "Not exist");
         require(msg.value >= orders[_tokenId].price, "Must be greater than price");
 
@@ -49,7 +65,7 @@ contract MarketPlace is Ownable {
         // TODO add event
     }
 
-    function cancel(uint256 _tokenId) public {
+    function cancel(uint256 _tokenId) public onlyOrderByType(_tokenId, OrderType.FIX_PRICE) {
         // TODO check owner
         require(orders[_tokenId], "Not exist");
         require(orders[_tokenId].status == OrderStatus.PENDING, "Bids already placed or order finished");
@@ -59,19 +75,33 @@ contract MarketPlace is Ownable {
         // TODO add event
     }
 
-    // function listItemOnAuction(uint256 _tokenId, uint256 _minPrice) public view returns (string memory) {
-    //     return greeting;
-    // }
+    function listItemOnAuction(uint256 _tokenId, uint256 _minPrice) public {
+        require(orders[_tokenId], "Already exist");
 
-    // function makeBid(uint256 _tokenId, uint256 _price) public view returns (string memory) {
-    //     return greeting;
-    // }
+        orders[_tokenId] = new Order(OrderType.AUCTION, OrderStatus.PENDING, _minPrice, msg.sender, block.timestamp);
+        transferNFT(msg.sender, address(this), _tokenId);
+        // TODO add event
+    }
 
-    // function finishAuction(uint256 _tokenId) public view returns (string memory) {
-    //     return greeting;
-    // }
+    function makeBid(uint256 _tokenId, uint256 _price) public onlyOrderByType(_tokenId, OrderType.AUCTION) {
+        uint256 lastBid = bids[_tokenId][bids[_tokenId].length - 1];
+        require(lastBid.amount < _price, "Bid amount must be greater than last bid");
 
-    // function cancelAuction() public view returns (string memory) {
-    //     return greeting;
-    // }
+        bids[_tokenId].push = new Bid(_price, msg.sender, block.timestamp);
+        transferFrom(address(this), lastBid.bidder, lastBid.amount);
+        transferFrom(msg.sender, address(this), _price);
+        // TODO add event
+    }
+
+    function finishAuction(uint256 _tokenId) public onlyOrderByType(_tokenId, OrderType.AUCTION) {
+        require(orders[_tokenId], "Not exist");
+        require(bids[_tokenId].length < 2, "Only if there are more than two bids");
+        require(orders[_tokenId].status != OrderStatus.FINISHED, "Already finished");
+        require(orders[_tokenId].createdAt + 3 days < block.timestamp, "It hasn't been three days");
+
+        orders[_tokenId].status = OrderStatus.FINISHED;
+        transferNFT(address(this), bids[_tokenId][bids[_tokenId].length - 1].bidder, _tokenId);
+        transferFrom(address(this), lastBid.bidder, lastBid.amount);
+        // TODO add event
+    }
 }
