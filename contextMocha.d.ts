dc906ea43d1113527e7ed6fd2e2ab5a7f30bfd81
bogdanaksenov <bogdanaks@bk.ru>
2022-03-17 01:08:41 +0300

    Update configs

---
+++ contextMocha.d.ts
@@ -0,0 +1,22 @@
+import {
+  MarketPlace,
+  MarketPlace__factory,
+  NFT,
+  NFT__factory,
+  Token,
+  Token__factory,
+} from "./typechain";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+
+declare module "mocha" {
+  export interface Context {
+    NFTContract: NFT__factory;
+    TokenContract: Token__factory;
+    MarketPlaceContract: MarketPlace__factory;
+    nft: NFT;
+    token: Token;
+    marketPlace: MarketPlace;
+    owner: SignerWithAddress;
+    addr1: SignerWithAddress;
+  }
+}
