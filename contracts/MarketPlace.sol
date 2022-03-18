43979165ab71f1cd2bed167aa6523a059400e6e1
bogdanaksenov <bogdanaks@bk.ru>
2022-03-18 13:05:12 +0300

    Del import console

--- contracts/MarketPlace.sol
+++ contracts/MarketPlace.sol
@@ -1,7 +1,6 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.11;
 
-import "hardhat/console.sol"; // TODO delete after use
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 import "./NFT.sol";
